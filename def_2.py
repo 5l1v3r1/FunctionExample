87db96100f30f8d1bc67b69df7ec852ab8b0ad81
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 16:48:10 +0300

    Update def_2.py

--- def_2.py
+++ def_2.py
@@ -1,1 +1,8 @@
-
+def russianmusic(**kwargs):
+  if "Serebro" in kwargs:
+    print("SerebroLove")
+    
+  else:
+    print(" :( : (:  ( " )
+    
+## russianmusic(Serebro=100,Madonna=50,JonasBrothers=5)
