b3aed640f09901c133a21cd1e7f7a913bfacf3cd
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-16 16:40:17 +0300

    Update def_1.py

--- def_1.py
+++ def_1.py
@@ -1,1 +1,13 @@
+def myfunction(number):
+	return number / 2 
 
+my_list = list(range(1,101))
+
+new_list = []
+for items in my_list:
+	new_list.append(myfunction(items))
+print(new_list)
+
+
+
+## My function examples 
